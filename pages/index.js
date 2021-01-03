1d4b8e745a83741338f4a0b0b5ee000c850b8d6f
Youri Wijnands <youri@wijnands.me>
2021-01-03 21:34:14 +0100

    Use the NextJS 10 Image component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -44,7 +45,7 @@ export default function Home() {
       <footer className="border-t border-gray-200">
         <a className="py-2 flex justify-center items-center" href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          <Image src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
