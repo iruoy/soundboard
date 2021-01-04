5e7572e5f48d46c18a9613c4e9ca969410d547a9
Youri Wijnands <youri@wijnands.me>
2021-01-04 13:26:48 +0100

    Add time displays

--- components/Sound.js
+++ components/Sound.js
@@ -1,40 +1,54 @@
-import { Component } from 'react';
+import {Component} from 'react';
 
 class Sound extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            progress: 0
-        }
+    this.state = {
+      currentTime: '00:00',
+      duration: '00:00',
+      progress: 0
     }
+  }
 
-    componentDidMount() {
-        this.audio = new Audio(this.props.src);
+  componentDidMount() {
+    this.audio = new Audio(this.props.src);
 
-        this.audio.addEventListener('timeupdate', (event) => {
-            this.setState({ progress: this.audio.currentTime / this.audio.duration * 100 });
-        });
-    }
+    this.audio.addEventListener('timeupdate', () => {
+      this.setState({
+        currentTime: this.format(this.audio.currentTime),
+        duration: this.format(this.audio.duration),
+        progress: this.audio.currentTime / this.audio.duration * 100
+      });
+    });
+  }
 
-    play() {
-        this.audio.fastSeek(0);
-        this.audio.play();
-    }
+  format(seconds) {
+    const date = new Date(seconds * 1000);
 
-    render() {
-        return (
-            <div className="m-4 p-6 text-left cursor-pointer select-none bg-gray-200 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-lg transition-colors hover:border-blue-500 focus:border-blue-500" onClick={this.play.bind(this)}>
-                <h3 className="mb-4 text-2xl font-bold">{this.props.title}</h3>
+    return date.getMinutes().toString().padStart(2, '0') + ':' + date.getSeconds().toString().padStart(2, '0');
+  }
 
-                <div className="relative pt-1">
-                    <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-300 dark:bg-blue-700">
-                        <div style={{ width: this.state.progress + '%' }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  play() {
+    this.audio.fastSeek(0);
+    this.audio.play();
+  }
+
+  render() {
+    return (
+      <div className="m-4 p-6 cursor-pointer select-none bg-gray-200 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-lg transition-colors hover:border-blue-500 focus:border-blue-500" onClick={this.play.bind(this)}>
+        <div className="mb-4 flex justify-between items-baseline">
+          <h2 className="text-2xl font-bold">{this.props.title}</h2>
+          <p className="font-mono">{this.state.currentTime} / {this.state.duration}</p>
+        </div>
+        <div className="relative">
+          <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-300 dark:bg-blue-700">
+            <div style={{width: this.state.progress + '%'}} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Sound;
--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,23 @@ import Head from 'next/head'
 import Sound from '../components/Sound'
 
 export default function Home() {
-    return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 antialiased">
-            <Head>
-                <title>Soundboard</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            
-            <div className="container mx-auto">
-                <h1 className="py-8 text-7xl font-bold text-center">Soundboard</h1>
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 antialiased">
+      <Head>
+        <title>Soundboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-                <div className="grid lg:grid-cols-2">
-                    <Sound title="Windows XP Startup" src="/sounds/windows xp/Windows XP Startup.wav" />
-                    <Sound title="Windows XP Shutdown" src="/sounds/windows xp/Windows XP Shutdown.wav" />
-                    <Sound title="Windows XP Recycle" src="/sounds/windows xp/Windows XP Recycle.wav" />
-                    <Sound title="Windows XP Error" src="/sounds/windows xp/Windows XP Error.wav" />
-                </div>
-            </div>
+      <div className="container mx-auto">
+        <h1 className="py-8 text-7xl font-bold text-center">Soundboard</h1>
+
+        <div className="grid lg:grid-cols-2">
+          <Sound title="Windows XP Startup" src="/sounds/windows xp/Windows XP Startup.wav"/>
+          <Sound title="Windows XP Shutdown" src="/sounds/windows xp/Windows XP Shutdown.wav"/>
+          <Sound title="Windows XP Recycle" src="/sounds/windows xp/Windows XP Recycle.wav"/>
+          <Sound title="Windows XP Error" src="/sounds/windows xp/Windows XP Error.wav"/>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
